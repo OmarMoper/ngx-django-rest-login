ec298ec32ed6b8c1f2705e5dbb91174310fb194b
Omar Mohamad - El Hassan Lopesino <omar.lopesino@metadrop.net>
2017-09-01 22:57:56 +0200

    Throw exception when user is already logged in

--- services/LoginService.ts
+++ services/LoginService.ts
@@ -3,16 +3,18 @@ import { Http, Headers, RequestOptions } from "@angular/http";
 import { DjangoUser } from '../models/DjangoUser';
 import {LoginTokenManager} from './LoginTokenManager';
 import "rxjs/add/operator/map";
+import {UserAlreadyLoggedInError} from '../models/Error';
 
 @Injectable()
 export class LoginService {
-  
+
   constructor(private http: Http) { }
 
   public authenticate(url, user : DjangoUser) {
+    if (this.isLoggedIn()) {
+      throw new UserAlreadyLoggedInError();
+    }
     let json = user.toJson();
-    console.log(url);
-    console.log(json);
     let options = this.getRequestOptions();
     return this.http.post(url, json, options).map((res) => res.json());
   }
